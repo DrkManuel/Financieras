2e4f2b5fed15738466c77edfffb521d6d44db7bd
Manuel Chaverra <manuelr16c@hotmail.com>
2018-10-25 14:48:34 -0500

    Creación del proyecto

---
+++ Financieras.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Financieras.Web.MvcApplication" Language="C#" %>
---
+++ Financieras.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Financieras.Web.Startup))]
+namespace Financieras.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
